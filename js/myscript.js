798387d7c5fc5320f13d989ccd9b5ec376a7cfbe
lailakh <lailakhas@gmail.com>
2018-05-04 13:06:00 +0300

    upload js file and it's code to working

--- js/myscript.js
+++ js/myscript.js
@@ -26,3 +26,21 @@ btn_nav.onclick=function(){
 
 
 
+ wow = new WOW(
+      {
+        animateClass: 'animated',
+        offset:       100,
+        callback:     function(box) {
+          console.log("WOW: animating <" + box.tagName.toLowerCase() + ">")
+        }
+      }
+    );
+    wow.init();
+    document.getElementById('moar').onclick = function() {
+      var section = document.createElement('section');
+      section.className = 'section--purple wow fadeInDown';
+      this.parentNode.insertBefore(section, this);
+    };
+
+
+
